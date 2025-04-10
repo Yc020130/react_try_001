b3d1a22c5933e794082778a89813f955486d43c0
Jared## <1922843603@qq.com>
2025-04-10 16:48:33 +0800

    added hello component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Hello from './hello.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>React学习001</h1>
+        <Hello />
       </header>
     </div>
   );
